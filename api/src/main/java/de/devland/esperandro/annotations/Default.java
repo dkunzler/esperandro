b453c1ea1a4c02c8cee7fabe130fdbe6ef794dba
dk <dk@davidkunzler.de>
2013-06-08 16:39:45 +0200

    License

--- api/src/main/java/de/devland/esperandro/annotations/Default.java
+++ api/src/main/java/de/devland/esperandro/annotations/Default.java
@@ -4,17 +4,29 @@ import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
+/*
+ * Copyright 2013 David Kunzler
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
 
 /**
- * Allows to set a default for the primitive preferences as well as {@code String}. A {@code Set<String>} will
- * always have {@code null} as default value;
+ * Allows to set a default for the primitive preferences as well as {@code String}. A {@code Set<String>} will always
+ * have {@code null} as default value;
  * <p/>
- * If no annotation is given or the proper default value is not set the used defaults are:
- * {@code boolean}: false
- * {@code int}: -1
- * {@code long}: -1l
- * {@code float}: -1.0f
- * {@code String}: "" (empty String)
+ * If no annotation is given or the proper default value is not set the used defaults are: {@code boolean}: false {@code
+ * int}: -1 {@code long}: -1l {@code float}: -1.0f {@code String}: "" (empty String)
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.SOURCE)
--- api/src/main/java/de/devland/esperandro/annotations/SharedPreferences.java
+++ api/src/main/java/de/devland/esperandro/annotations/SharedPreferences.java
@@ -7,7 +7,22 @@ import java.lang.annotation.Target;
 
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.SOURCE;
-
+/*
+ * Copyright 2013 David Kunzler
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
 @Target(TYPE)
 @Retention(SOURCE)
 public @interface SharedPreferences {
