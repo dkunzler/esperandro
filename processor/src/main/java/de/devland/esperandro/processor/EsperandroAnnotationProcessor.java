882d39b933b512b81e479af1e9184b9796094944
David Kunzler <dk@devland.de>
2013-11-15 22:14:29 +0100

    add SuppressWarnings to generated classes

--- processor/src/main/java/de/devland/esperandro/processor/EsperandroAnnotationProcessor.java
+++ processor/src/main/java/de/devland/esperandro/processor/EsperandroAnnotationProcessor.java
@@ -190,6 +190,7 @@ public class EsperandroAnnotationProcessor extends AbstractProcessor {
             result.emitPackage(packageName);
             result.emitImports(neededImports);
             result.emitEmptyLine();
+            result.emitAnnotation(SuppressWarnings.class, new String[]{"\"all\""});
             result.beginType(typeName + SUFFIX, "class", modPublic, null, qualifiedNameable.getQualifiedName()
                     .toString(), SharedPreferenceActions.class.getName());
             result.emitEmptyLine();
