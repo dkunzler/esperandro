2a1af1a6579cdb91dc36fa6e5177a8ded261c0ee
David Kunzler <dk@devland.de>
2016-01-01 23:27:21 +0100

    add test stubs for future tests

---
+++ tests/src/test/java/de/devland/esperandro/NameModeTest.java
@@ -0,0 +1,16 @@
+package de.devland.esperandro;
+
+/**
+ * Created by deekay on 17.07.2015.
+ */
+
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+@Config(manifest = Config.NONE)
+@RunWith(RobolectricTestRunner.class)
+@Ignore(value = "TODO: add tests")
+public class NameModeTest {
+}
---
+++ tests/src/test/java/de/devland/esperandro/SharedPreferenceActionsTest.java
@@ -0,0 +1,16 @@
+package de.devland.esperandro;
+
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+/**
+ * Created by deekay on 17.07.2015.
+ */
+@Config(manifest = Config.NONE)
+@RunWith(RobolectricTestRunner.class)
+@Ignore(value = "TODO: add tests")
+public class SharedPreferenceActionsTest {
+
+}
