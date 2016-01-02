3a932cd9a70e299ea34975960f25ca8712bb221f
David Kunzler <dk@devland.de>
2016-01-02 21:54:00 +0100

    javadoc

--- api/src/main/java/de/devland/esperandro/annotations/Cached.java
+++ api/src/main/java/de/devland/esperandro/annotations/Cached.java
@@ -1,8 +1,5 @@
 package de.devland.esperandro.annotations;
 
-import android.annotation.TargetApi;
-import android.os.Build;
-
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
@@ -10,13 +7,29 @@ import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 /**
- * Enabled caching on SharedPreference
+ * Enabled caching on SharedPreference interface.
+ * IMPORTANT: caching does not reflect changes made via Android internal methods of altering SharedPreference files.
+ * Only changes made via esperandro generated classes can be reflected in the cache. Extend
+ * {@link de.devland.esperandro.CacheActions} in your interface to interact directly with the cache.
+ * It is not possible to use this annotation on an interface for the default preferences since it is very likely that
+ * these are changed in {@link android.preference.PreferenceActivity} oder {@link android.preference.PreferenceFragment}.
  */
 @Target(TYPE)
 @Retention(RUNTIME)
-@TargetApi(Build.VERSION_CODES.HONEYCOMB_MR1)
 public @interface Cached {
+    /**
+     * defines the size of the underlying cache
+     */
     int cacheSize() default 20;
+
+    /**
+     * defines if values are updated when a put occurs (otherwise value in cache is deleted on put)
+     */
     boolean cacheOnPut() default false;
+
+    /**
+     * indicates if LruCache from support library v4 is used or the LruCache integrated in the
+     * Android SDK (version 12 and up)
+     */
     boolean support() default false;
 }
